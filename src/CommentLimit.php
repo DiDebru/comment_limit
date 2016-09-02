6047a888735e3f299ec85a865fc15908873a2bc0
Insasse <dirk.schmidt@erdfisch.de>
2016-09-02 14:27:04 +0200

    Create private function to get FieldConfig and make some code style related chan ged

--- src/CommentLimit.php
+++ src/CommentLimit.php
@@ -28,33 +28,31 @@ class CommentLimit {
   protected $database;
 
   /**
-   * FieldConfig of the current comment field.
-   *
-   * @var FieldConfig $comment
-   */
-  protected $comment;
-
-  /**
    * Constructor.
    */
-  public function __construct(Connection $database, AccountProxyInterface $user, FieldConfig $comment) {
+  public function __construct(Connection $database, AccountProxyInterface $user) {
     $this->database = $database;
     $this->user = $user;
-    $this->comment = $comment;
   }
 
   /**
    * Get user comment limit for this user.
    *
+   * @param int $entity_id
+   *   The ID of the current entity.
+   * @param string $entity_type
+   *   Current entity type.
+   *
    * @return int
    *    Current count of comments the user has made on an entity.
    */
-  public function getCurrentCommentCountForUser($entityId) {
+  public function getCurrentCommentCountForUser($entity_id, $entity_type) {
     // Count comment of user.
     $query = $this->database->select('comment_field_data', 'c')
       ->fields('c', ['entity_id', 'uid'])
       ->condition('uid', $this->user->id())
-      ->condition('entity_id', $entityId)
+      ->condition('entity_id', $entity_id)
+      ->condition('entity_type', $entity_type)
       ->execute();
     $query->allowRowCount = TRUE;
     return $query->rowCount();
@@ -63,13 +61,19 @@ class CommentLimit {
   /**
    * Get node comment limit for this entity.
    *
+   * @param int $entity_id
+   *   The ID of the current entity.
+   * @param string $entity_type
+   *   Current entity type.
+   *
    * @return int
    *    Current count of comments that were made on an entity.
    */
-  public function getCurrentCommentsOnEntity($entityId) {
+  public function getCurrentCommentsOnEntity($entity_id, $entity_type) {
     $query = $this->database->select('comment_entity_statistics', 'c')
       ->fields('c', ['comment_count'])
-      ->condition('entity_id', $entityId)
+      ->condition('entity_id', $entity_id)
+      ->condition('entity_type', $entity_type)
       ->execute()
       ->fetchField();
     return $query;
@@ -78,34 +82,49 @@ class CommentLimit {
   /**
    * Get the comment limit of the entity.
    *
+   * @param int $entity_id
+   *   The ID of the current entity.
+   * @param string $entity_type
+   *   Current entity type.
+   *
    * @return mixed|null
    *   Returns the comment limit of the entity.
    */
-  public function getEntityLimit() {
-    return $this->comment->getThirdPartySetting('comment_limit', 'edit-limit-per-entity', FALSE);
+  public function getEntityLimit($entity_id, $entity_type) {
+    $commentLimit = $this->getFieldConfig($entity_id, $entity_type);
+    return $commentLimit->getThirdPartySetting('comment_limit', 'edit-limit-per-entity', FALSE);
   }
 
   /**
    * Get the comment limit for the user.
    *
+   * @param int $entity_id
+   *   The ID of the current entity.
+   * @param string $entity_type
+   *   Current entity type.
+   *
    * @return mixed|null
    *   Returns the comment limit for the user.
    */
-  public function getUserLimit() {
-    return $this->comment->getThirdPartySetting('comment_limit', 'edit-limit-per-user', FALSE);
+  public function getUserLimit($entity_id, $entity_type) {
+    return $this->getFieldConfig($entity_id, $entity_type)->getThirdPartySetting('comment_limit', 'edit-limit-per-user', FALSE);
   }
 
   /**
    * Has the user reached his/her comment limit.
    *
-   * @param int $entityId
+   * @param int $entity_id
    *   The ID of the current entity.
+   * @param string $entity_type
+   *   Current entity type.
    *
    * @return bool
    *    Returns TRUE or FALSE.
    */
-  public function hasUserLimitReached($entityId) {
-    if ($this->getCurrentCommentCountForUser($entityId) <= $this->getUserLimit() && $this->user->hasPermission('bypass comment limit')) {
+  public function hasUserLimitReached($entity_id, $entity_type) {
+    $test1 = $this->getCurrentCommentCountForUser($entity_id, $entity_type);
+    $test3 = $this->getUserLimit($entity_id, $entity_type);
+    if ($this->getCurrentCommentCountForUser($entity_id, $entity_type) >= $this->getUserLimit($entity_id, $entity_type) && !$this->user->hasPermission('bypass comment limit')) {
       return TRUE;
     }
     else {
@@ -116,19 +135,41 @@ class CommentLimit {
   /**
    * Has the comment limit for the entity been reached.
    *
-   * @param int $entityId
+   * @param int $entity_id
    *    The ID of the current entity.
+   * @param string $entity_type
+   *   Current entity type.
    *
    * @return bool
    *    Returns TRUE or FALSE.
    */
-  public function isEntityLimitReached($entityId) {
-    if ($this->getCurrentCommentsOnEntity($entityId) <= $this->getEntityLimit() && $this->user->hasPermission('bypass comment limit')) {
+  public function hasEntityLimitReached($entity_id, $entity_type) {
+    $test = $this->getCurrentCommentsOnEntity($entity_id, $entity_type);
+    $test2 = $this->getEntityLimit($entity_id, $entity_type);
+    if ($this->getCurrentCommentsOnEntity($entity_id, $entity_type) >= $this->getEntityLimit($entity_id, $entity_type) && !$this->user->hasPermission('bypass comment limit')) {
       return TRUE;
     }
     else {
       return FALSE;
     }
+  }
+
+  /**
+   * Get the FieldConfig of a comment field used in a specific entity bundle.
+   *
+   * @param int $entity_id
+   *   Current entity id.
+   * @param string $entity_type
+   *   Current entity type.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null|static
+   *    Returns the FieldConfig object.
+   */
+  private function getFieldConfig($entity_id, $entity_type) {
+    $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($entity_id);
+    $entity_bundle = $entity->bundle();
+    $field_config = FieldConfig::load($entity_type . '.' . $entity_bundle . '.comment');
+    return $field_config;
   }
 
 }
